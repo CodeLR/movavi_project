cf88015e7e29f7df55337a79ce728affdd56fc20
Codelr <codelr2017@gmail.com>
2020-09-11 12:17:23 +0300

    add exeption for File

---
+++ fileopen.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <filesystem>
+#include <string.h>
+using namespace std;
+void MyFile(const char*);
+int main()
+{
+	try
+	{
+		MyFile("C:/qtProject/Work/fileopen/mytext.txt");
+		//MyFile("Ваша директория");
+	}
+	catch (invalid_argument&e)
+	{
+		cerr << e.what() << endl;
+		cout << "Exeption";
+		return -1;
+	}
+}
+void MyFile(const char * name)
+{
+	FILE* fp = fopen(name, "w");
+	if (name == "") {
+		throw invalid_argument("argument");
+	}
+	else {
+		fprintf(fp, "Rodion Loskutov");
+	}
+	// возможны и другие проверки
+	fclose(fp);
+}
