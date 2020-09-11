1636d16e8c99cc57e01c2c839155c607499cc55e
Codelr <codelr2017@gmail.com>
2020-09-11 12:27:41 +0300

    11.09.2020

--- Work/massive_poisk/massive_poisk/massive_poisk.cpp
+++ Work/massive_poisk/massive_poisk/massive_poisk.cpp
@@ -5,7 +5,7 @@
 #include <iterator>
 
 using namespace std;
-
+void Foo(vector<int>& vec);
 int main()
 {
     std::vector<int>myVec;
@@ -13,31 +13,34 @@ int main()
     myVec.push_back(10);
     myVec.push_back(3);
     myVec.push_back(43);
+    myVec.push_back(43);
+    myVec.push_back(43);
     myVec.push_back(65);
     myVec.push_back(2);
     myVec.push_back(18);
     myVec.push_back(44);
     myVec.push_back(65);
     myVec.push_back(3);
+    myVec.push_back(43);
     myVec.push_back(99);
-    
-
-    // 10 , 43 , 2 , 18 , 44 , 99 
-   
-    for (int i = 0; i < myVec.size(); ++i)
+    Foo(*&myVec);
+    for (int i = 0; i < myVec.size(); ++i) {
+        cout << myVec[i] << " ";
+    }
+}
+void Foo(vector<int>& vec) {
+    for (int i = 0; i < vec.size(); ++i)
     {
-        for (int j = 0; j < myVec.size(); ++j)
-        {   
-            if (myVec[i] == myVec[j] && i != j)
+        for (int j = 0; j < vec.size(); ++j)
+        {
+            if (vec[i] == vec[j] && i != j)
             {
-                myVec.erase(myVec.begin() + j);
-                j--; 
+                cout << i <<endl;
+                vec.erase(vec.begin() + j);
+                j--;
             }
         }
     }
-            for (int i = 0; i < myVec.size(); ++i) {
-            cout << myVec[i] << " ";
-        }
 }
 
 
